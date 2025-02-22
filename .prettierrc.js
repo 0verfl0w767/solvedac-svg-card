19237dd5c439747396796f26f3f1cad595e9a0c1
0verfl0w767 <98698629+0verfl0w767@users.noreply.github.com>
2025-02-22 11:20:58 +0900

    chore: Remove unnecessary file

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  quoteProps: 'preserve',
-  printWidth: 100,
-  semi: false,
-  trailingComma: 'all',
-}
